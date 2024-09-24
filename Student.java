8ae7d40652545013eb85ecec98ba037793b546e4
Sofia Larina <lar240747@spengergasse.at>
2024-09-24 12:08:05 +0200

    Construktor hinzgefügt public Student(String neuName, int neuAlter, String neuReligion, boolean neuMatura)

--- Student.java
+++ Student.java
@@ -3,8 +3,17 @@ public class Student
     private String name;
     private int alter;
     private String religion;
+    private boolean matura;
+    
+    public Student(String neuName, int neuAlter, String neuReligion, boolean neuMatura)
+    {
+       setName(neuName);
+       setAlter(neuAlter);
+       setReligion(neuReligion);
+       setMatura(neuMatura);
+    }
 
-        public void setName(String neuName)
+    public void setName(String neuName)
     {
         name = neuName;
     }
@@ -17,6 +26,11 @@ public class Student
     public void setReligion(String neuReligion)
     {
         religion = neuReligion;
+    }
+    
+    public void setMatura(boolean neuMatura)
+    {
+        matura = neuMatura;
     }
 
     public String getName()
@@ -32,6 +46,11 @@ public class Student
     public String getReligion()
     {
         return religion;
+    }
+    
+    public boolean getMatura()
+    {
+        return matura;
     }
 
 }
